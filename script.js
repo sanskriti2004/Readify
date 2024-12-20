af9b480cafd18c61e0540a5e160feb774f153274
sanskriti2004 <sanskritigogoi2004@gmail.com>
2024-12-20 16:13:30 +0530

    added styling effects

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const formCloseButton = document.querySelector("#form-close-btn");
 const formDiv = document.querySelector(".book-info");
+const addBookButton = document.querySelector("#add-btn");
+const mainContainer = document.querySelector(".content");
+
+formCloseButton.addEventListener("click", () => {
+  formDiv.style.display = "none";
+  mainContainer.classList.remove("blurred");
+});
+addBookButton.addEventListener("click", () => {
+  formDiv.style.display = "flex";
+  mainContainer.classList.add("blurred");
+});
